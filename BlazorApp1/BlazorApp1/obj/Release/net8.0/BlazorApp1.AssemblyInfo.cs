082767f375964b9f5538f87a710d65e1a1e0c1ee
vgdobon <vgdobon91@gmail.com>
2024-02-05 00:33:16 +0100

    ACT

--- BlazorApp1/BlazorApp1/obj/Release/net8.0/BlazorApp1.AssemblyInfo.cs
+++ BlazorApp1/BlazorApp1/obj/Release/net8.0/BlazorApp1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2412cbd6d73cd31f34e3320cdec18f996851ef81")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
